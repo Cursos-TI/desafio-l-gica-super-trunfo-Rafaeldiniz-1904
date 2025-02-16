5c0592953ae20b0bc40eea26d9107092495d9728
Rafaeldiniz-1904 <202502006403@alunos.estacio.br>
2025-02-16 17:46:19 +0000

    Projeto cartas de paises 1

--- Projeto cartas.c
+++ Projeto cartas.c
@@ -2,15 +2,23 @@
 
 int main(){
 
-    char estado;
-    char codigo da carta [3];
-    char nome da cidade [20];
+    char estado = 'A';
+    char codigodacarta [10] = "01";
+    char nomedacidade [20] = "São Paulo";
     int populacao;
-    float area em km²;
+    float area;
     float pib;
-    int numero de pontos turisticos;
+    int turisticos;
 
+    printf("*** CARTA BRASIL ***\n");
 
-    
+    printf("Estado:%c\n", estado);
+    printf("Código da carta:%c%s\n", estado, codigodacarta);
+    printf("Nome da Cidade:%s\n", nomedacidade);
+    printf("População:%i bilhões de Pessoas\n", populacao);
+    printf("Àrea em km²:%f km²\n", area);
+    printf("PIB:%i Biihões de reais\n", pib);
+    printf("Numeros de pontos turisticos:%d \n", turisticos);
+
     return 0;
 }
